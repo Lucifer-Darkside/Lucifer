087dfc4df0de49449a96304f233a04fbf92df56d
Lucifer-Darkside <60489993+Lucifer-Darkside@users.noreply.github.com>
2020-01-31 08:40:38 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const  Discord = require ("discord.js").
+const bot= new Discord.client().
+const cfg= require (./index.lsg);
+const  prefix = (/)
+
+bot.on(ready,function(){
+  console.log (Je suis prêt àêtre utilisé.)
+))
+
+
+bot.login(cfg.token)
